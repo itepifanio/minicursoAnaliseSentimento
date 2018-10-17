5169f67ed2a0fe391ed1a98fc26a3fe5beec7a39
Ítalo Epifâniop <italoepifaniols@gmail.com>
2018-10-17 09:58:44 -0300

    Simples analisador de sentimentos de tweets pronto

--- src/sentimentAnalysis.py
+++ src/sentimentAnalysis.py
@@ -3,6 +3,11 @@ import numpy as np
 from textblob import TextBlob
 from settings import *
 
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
 # Infelizmente a biblioteca textblob tá bugada
 # e não conseguiremos utilizar o google tradutor
 # para traduzir do inglês pro português :(
@@ -15,16 +20,11 @@ def is_english(text):
     return False
 """
 
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token,access_token_secret)
+def tweet_analisys(query):
+    tweets = tweepy.Cursor(api.search, q=query + " -filter:retweets").items(20)
 
-api = tweepy.API(auth)
-
-# Buscando tweets relacionados a python brasil ignorando retweets
-tweets = api.search('Python Brasil -filter:retweets')
-
-def tweet_analisys():
     polarities = []
+    subjectivities = []
 
     # Iterando os resultados e passando para o textblob
     for tweet in tweets:
@@ -35,18 +35,38 @@ def tweet_analisys():
 
         if (phrase.sentiment.polarity != 0.0 and phrase.sentiment.subjectivity != 0.0):
             polarities.append(phrase.sentiment.polarity)
+            subjectivities.append(phrase.sentiment.subjectivity)
 
-
+        """
         print('Tweet: ' + tweet.text)
         print('Polarity: ' + str(phrase.sentiment.polarity) + " \ " + str(phrase.sentiment.subjectivity))
         print('.....................')
+        """
+        
+    return {'polarity':polarities, 'subjectivity':subjectivities}
 
-    return polarities
+# Utiliza uma media ponderada da polaridade utilizando a subjetividade como peso
+# textos subjetivos (com mais emoção) terão peso maior
+def get_weighted_polarity_mean(valid_tweets):
+    return np.average(valid_tweets['polarity'],weights=valid_tweets['subjectivity'])
 
-polarity_mean = np.mean(tweet_analisys())
+def get_polarity_mean(valid_tweets):
+    return np.mean(valid_tweets['polarity'])
 
-print('Média: ' + str(polarity_mean))
-if(polarity_mean > 0.0):
-    print('POSITIVE')
-else:
-    print('NEGATIVE')
+def print_result(mean):
+    if mean > 0.0:
+        print('POSITIVE')
+    elif mean == 0.0:
+        print('NEUTRO')
+    else:
+        print('NEGATIVE')
+
+if __name__ == "__main__":
+    query = input("Entre a query de analise: ")
+    analysis = tweet_analisys(query)
+
+    print('MÉDIA PONDERADA: ' + str(get_weighted_polarity_mean(analysis)))
+    print_result(get_weighted_polarity_mean(analysis))
+
+    print('MÉDIA: ' + str(get_polarity_mean(analysis)))
+    print_result(get_polarity_mean(analysis))
